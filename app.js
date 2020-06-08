09a67b8fcd61482e6450810863c33c39eb6d3989
romasinha <sinha.roma99@gmail.com>
2020-06-08 17:36:10 +0530

    commit

--- app.js
+++ app.js
@@ -1,2 +1,63 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
+const greeting = ["I am good sweet heart",
+                 "I don't love you, okay!",
+                 "You are such a loser!",
+                 "I hate you from the core"];
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+// when the recognition starts
+recognition.onstart = () => {
+    
+};
+
+recognition.onresult = function(event){
+    const current = event.resultIndex;
+
+    // capturing transcript-text
+    const transcript = event.results[current][0].transcript
+    content.textContent = transcript;
+    readOutLoud(transcript);
+}
+
+// Add Listener to the talk button
+btn.addEventListener('click', () =>{
+    recognition.start();
+})
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+
+    //Default text
+
+    speech.text = "Sorry, can you try again?";
+
+    // If the speech contains "I love"
+
+    if(message.includes("I love")){
+        const finalText = 
+        greeting[Math.floor(Math.random()*greeting.length)];
+        speech.text = finalText;
+    }
+
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    
+
+
+    speech.lang = 'en-US';
+
+    window.speechSynthesis.speak(speech);
+}
+
+
+
+
+
+
+
+
